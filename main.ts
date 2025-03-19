e772d192c0aae23dbd357f43c106c502f138e95a
Hayden Mankin <DryCreations@gmail.com>
2025-03-19 20:24:41 +0000

    test block

--- main.ts
+++ main.ts
@@ -1,1 +1,12 @@
 
+//% weight=100 color=#0fbc11 icon="\uf1b2" block="Scene Manager"
+namespace sceneManager {
+    /**
+     * Say hello to the user.
+     * @param name The name of the user
+     */
+    //% block
+    export function sayHello(name: string): void {
+        console.log(`Hello, ${name}!`);
+    }
+}
